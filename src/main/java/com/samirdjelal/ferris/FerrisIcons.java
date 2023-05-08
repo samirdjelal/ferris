b877d984c6a9d4fef35ddee0b63cba838cc63d95
Samir Djelal <ssdjelal@gmail.com>
2023-05-08 20:56:50 +0100

    fix compatibility issues

--- src/main/java/com/samirdjelal/ferris/FerrisIcons.java
+++ src/main/java/com/samirdjelal/ferris/FerrisIcons.java
@@ -5,5 +5,5 @@ import com.intellij.openapi.util.IconLoader;
 import javax.swing.*;
 
 public interface FerrisIcons {
-    Icon FERRIS_ICON = IconLoader.getIcon("/rsz_ferris.png");
+    Icon FERRIS_ICON = IconLoader.getIcon("/rsz_ferris.png", FerrisIcons.class);
 }
